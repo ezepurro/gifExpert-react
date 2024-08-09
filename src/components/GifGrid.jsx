b2dae79cafaecacca91f7a08764eb9f28fd8ac42
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-08-09 15:48:57 -0300

    Primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,33 @@
+import GifGridItem from "./GifGridItem";
+import useFetchGifs from "../hooks/useFetchGifs";
+
+
+
+export const GifGrid = ({category}) => {
+
+  const {images, isLoading} = useFetchGifs(category);
+
+  return (
+    <>
+        <h3>{category}</h3>
+        {
+          isLoading
+          ? ( <h2>Loading...</h2> )
+          : null
+        }
+        <div className="card-grid">
+            {          
+              images.map( image => (
+                  <GifGridItem
+                    key={image.id}
+                    {...image}
+                  />
+              ))
+            }
+        </div>
+
+    </>
+  )
+}
+
+
